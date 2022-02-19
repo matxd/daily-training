c81ed9e8d4c438f4f047c94e715afbb012364270
matxd <76856999+matxd@users.noreply.github.com>
2022-02-19 19:30:23 -0300

    array

--- JavaScript/array.js
+++ JavaScript/array.js
@@ -6,6 +6,7 @@ let arrayDois = [3, 2, 1]
 
 console.log([...array, ...arrayDois]) // ... concatena os valores dos arrays em um novo array
 console.log(array.slice(1, 2)) // SLICE necessita de dois indices sem incluir o indice final no console
+console.log(array.splice(0, 1)) // SPLICE necessita de dois indices, onde vai começar a remover e onde vai parar
 console.log(array.pop()) // POP remove o ultimo elemento do array
 console.log(array.push("expoit")) // PUSH adiciona um elemento na ultima posição do array
 console.log(array.includes("xdm")) // INCLUDES verifica se o valor chamado existe no array
