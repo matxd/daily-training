a2591c5106336a814dc9be8c63b21c5957faf52a
matxd <76856999+matxd@users.noreply.github.com>
2022-02-21 17:13:08 -0300

    node

--- Node.js/stats.js
+++ Node.js/stats.js
@@ -1,18 +1,18 @@
 const os = require("os")
-const exec = require("./logger") // requisitando uma função no arquivo "logger"
+const exec = require("./events") // requisitando uma função no arquivo "logger"
 
 setInterval(() => { // setInterval com uma ArrowFunction passando todo o bloco de código
     const { freemem, totalmem } = os
     const free = parseInt(freemem() / 1024 / 1024)
     const total = parseInt(totalmem() / 1024 / 1024)
     const usage = parseInt((1 - free / total) * 100)
-    const stat = {
+    const status = {
         free: `${free} MB`,
         total: `${total} MB`,
         usage: `${usage}%`
     }
 
 console.clear()
-console.table(stat)
-exec("Rodando...")
+console.table(status)
+exec(`${JSON.stringify(status)}\n`) // obj JavaScript transformado em obj JSON formatado em string
 }, 1000) // setando o tempo em milissegundos
