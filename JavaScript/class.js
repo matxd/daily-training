6231b8296922624f10a646419eba5f57c72919a9
matxd <76856999+matxd@users.noreply.github.com>
2022-03-12 18:25:49 -0300

    class basic

---
+++ JavaScript/class.js
@@ -0,0 +1,12 @@
+class Carro {
+    constructor(nome, ano){
+        this.nome = nome
+        this.ano = ano
+    }
+}
+
+const carroUm = new Carro("BMW X6", 2021)
+const carroDois = new Carro("Audi A4", 2019)
+
+console.log(carroUm)
+console.log(carroDois)
