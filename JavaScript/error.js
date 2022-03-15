8a21b4575c6295ffaad068afe04a964e9a3f1192
matxd <matheusrcrv07@gmail.com>
2022-03-15 15:18:43 -0300

    throw error

--- JavaScript/error.js
+++ JavaScript/error.js
@@ -1,11 +1,25 @@
 const errorState = () => {
     try { // try tenta executar o bloco de código
-        forceError
-    } catch (error) {
-        console.error(`Capturando e manipulando esse erro \n${error}`)
+        random
+    } catch (error) { // captura o erro
+        console.error(`Capturando e tratando esse erro \n${error}`)
     } finally {
         console.log("Sempre será executado")
     }
 }
 
 errorState()
+
+
+const forceError = () => {
+    const valorProibido = true
+    try {
+        if(valorProibido){
+            throw new Error("Não insira esse valor!")
+        }
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+forceError()
