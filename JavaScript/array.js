7a7a8ab629e0a6247881bfe74e1fed15990d1f3c
matxd <matheusrcrv07@gmail.com>
2022-03-21 17:33:21 -0300

    map, filter, reduce

--- JavaScript/array.js
+++ JavaScript/array.js
@@ -12,9 +12,23 @@ words.pop() // remove o elemento da ultima posição
 words.push("Expoit") // adiciona o elemento na ultima posição
 words.includes("Xdm") // verifica se o valor existe
 
-words.map(word => word.toLowerCase())
-numbers.map(number => number * 2) // itera sobre o array e mapeia o que foi pedido retornando um novo array mapeado
-numbers.filter(number => number % 2 === 0) // itera sobre o array e filtra o que foi pedido
+words.filter((primeiraLetra) => primeiraLetra[0] === "M")
+words.map((word) => word.toLowerCase())
+numbers.map((number) => number * 2) // itera sobre o array e retorna o array modificado
+numbers.filter((number) => number % 2 === 0) // itera sobre o array e filtra o que foi pedido retornando um novo array
 numbers.reduce((vAcumulador, vAtual) => vAcumulador + vAtual, 0) // itera sobre o array e reduz de acordo com o codigo, tendo como segundo argumento o valor inicial do acumulador
 
 numbers.sort((a, b) => a - b)
+
+let alunos = [
+    {nome: "matHeus", idade: 16, nota: 8.5},
+    {nome: "FelIpe", idade: 15, nota: 9.3},
+    {nome: "gustAVo", idade: 14, nota: 3.2},
+    {nome: "REnAn", idade: 17, nota: 5.5}
+]
+
+let media = 6
+
+alunos.filter((valor) => valor.nota >= media)
+alunos.filter((valor) => valor.nota < media)
+alunos.map((maiusc) => Object.assign(maiusc, {nome: maiusc.nome.toUpperCase()}))
